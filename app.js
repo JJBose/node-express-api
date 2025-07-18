770b10f91c9055d8bf7debbce89fd78abfda7a16
Jayesh Srivastava <jayesh.srivastava2@globallogic.com>
2025-07-18 12:23:32 +0530

    fixed logger and rate limiter middleware

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const healthRoutes = require('./routes/health');
-const authRoutes = require('./routes/auth');
-const feedRoutes = require('./routes/feeds');
-const rateLimiter = require('./middlewares/rateLimiter');
-const logger = require('./middlewares/logger');
+const express = require("express");
+const healthRoutes = require("./routes/health");
+const authRoutes = require("./routes/auth");
+const feedRoutes = require("./routes/feeds");
+const rateLimiter = require("./middlewares/rateLimiter");
+const logger = require("./middlewares/logger");
 
 const app = express();
 const PORT = 3000;
@@ -11,11 +11,11 @@ const PORT = 3000;
 app.use(express.json());
 
 // Routes
-app.use('/health', healthRoutes);
-app.use('/', authRoutes);
-app.use('/feeds', feedRoutes);
 app.use(rateLimiter);
 app.use(logger);
+app.use("/health", healthRoutes);
+app.use("/", authRoutes);
+app.use("/feeds", feedRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
